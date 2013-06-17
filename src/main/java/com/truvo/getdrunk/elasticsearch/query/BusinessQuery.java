6b7658f05ba5cdb1108723080f220f2116fca2d1
bgossey <bgossey@bert-gosseys-MacBook-Pro.local>
2013-06-17 15:26:13 +0200

    dummy

--- src/main/java/com/truvo/getdrunk/elasticsearch/query/BusinessQuery.java
+++ src/main/java/com/truvo/getdrunk/elasticsearch/query/BusinessQuery.java
@@ -7,10 +7,11 @@ import org.elasticsearch.client.Client;
 import org.elasticsearch.node.Node;
 
 import com.truvo.getdrunk.web.Query;
+import com.truvo.getdrunk.web.QueryResponse;
 
 public class BusinessQuery {
 
-	public void query(Query query) {
+	public QueryResponse query(Query query) {
 
 		Node node = nodeBuilder().clusterName("InDomoCluster").node();
 
@@ -19,6 +20,14 @@ public class BusinessQuery {
 		// final SearchRequestBuilder searchRequestBuilder = client.prepareSearch("businesses");
 		SearchResponse response = client.prepareSearch("businesses").setTypes("nl").setFrom(0).setSize(query.getMaxResults()).execute().actionGet();
 
+		return convertResponse(response);
+
+	}
+
+	private QueryResponse convertResponse(SearchResponse response) {
+		// response
+
+		return null;
 	}
 
 }
