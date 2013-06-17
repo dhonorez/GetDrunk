5fa19737865895287e8c5982f9a8e4a2d77b7ae3
Dylan Honorez <dylan.honorez@gmail.com>
2013-06-17 12:11:57 +0200

    Added service & basic query object

---
+++ src/main/java/com/truvo/getdrunk/web/Coordinate.java
@@ -0,0 +1,30 @@
+package com.truvo.getdrunk.web;
+
+public class Coordinate {
+
+	private double x;
+	private double y;
+
+	public Coordinate(double x, double y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+}
---
+++ src/main/java/com/truvo/getdrunk/web/Query.java
@@ -0,0 +1,44 @@
+package com.truvo.getdrunk.web;
+
+import java.util.List;
+
+public class Query {
+
+	private String category;
+	private int maxResults;
+	private List<Coordinate> coordinates;
+	private boolean openNow;
+
+	public String getCategory() {
+		return category;
+	}
+
+	public int getMaxResults() {
+		return maxResults;
+	}
+
+	public List<Coordinate> getCoordinates() {
+		return coordinates;
+	}
+	
+	public boolean isOpenNow() {
+		return openNow;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public void setMaxResults(int maxResults) {
+		this.maxResults = maxResults;
+	}
+
+	public void setCoordinates(List<Coordinate> coordinates) {
+		this.coordinates = coordinates;
+	}
+	
+	public void setOpenNow(boolean openNow) {
+		this.openNow = openNow;
+	}
+
+}
